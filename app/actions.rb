cfc7d483537236f6a0ea3b2f9d53bdfe6d66cfbd
Laura Hutchinson <laura.hutchinson@cybera.ca>
2017-03-28 20:32:38 +0000

    w3d1 final

--- app/actions.rb
+++ app/actions.rb
@@ -10,12 +10,13 @@ get '/' do
     @post_shark = {
         username: "sharky_j",
         avatar_url: "http://naserca.com/images/sharky_j.jpg",
+        photo_url: "http://naserca.com/images/shark.jpg",
         humanized_time_ago: humanized_time_ago(15),
         like_count: 0,
         comment_count: 1,
         comments: [{
             username: "sharky_j",
-            text: "sharky_j: Out for the long weekend... too embarrassed to show y'all the beach bod!"
+            text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
         }]
     } 
         
@@ -45,7 +46,7 @@ get '/' do
        }]
    }
 
-    [@post_shark, @post_whale, @post_marlin].to_s
+    @posts = [@post_shark, @post_whale, @post_marlin]
 
-erb:index    
+    erb(:index)    
 end
